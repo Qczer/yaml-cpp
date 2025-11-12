7d49b2e81f8d7cde9c3b8285d434e97c74f95caf
Qczer <mateuszkuczera3@gmail.com>
2025-11-12 13:15:05 +0100

    Forked yaml-cpp

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "yaml-cpp"
 	kind "StaticLib"
 	language "C++"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -25,14 +26,12 @@ project "yaml-cpp"
 
 	filter "system:windows"
 		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "off"
+		cppdialect "C++23"
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "off"
+		cppdialect "C++23"
 
 	filter "configurations:Debug"
 		runtime "Debug"
